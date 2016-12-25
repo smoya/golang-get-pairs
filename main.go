60ea05d1c232587bc40b2b63a9bd56c061f79b55
Marcos <velozmarkdrea@gmail.com>
2016-12-25 21:42:30 +0100

    Init repository with tests

---
+++ main.go
@@ -0,0 +1,10 @@
+package pairs
+
+type StructForPairs struct {
+	values []int64
+	sum    int64
+}
+
+func GetPairsThatMatchesSum(pairs StructForPairs) (bool, []int64){
+	return true, []int64{4,4}
+}
---
+++ main_test.go
@@ -0,0 +1,47 @@
+package pairs
+
+import (
+	"testing"
+)
+
+type testStruct struct {
+	tests                  StructForPairs
+	expected_value         bool
+	expected_slice_of_ints []int64
+}
+
+var tests = []testStruct{
+	{
+		StructForPairs{[]int64{1, 2, 4, 9}, 8}, false, nil,
+	},
+	{
+		StructForPairs{[]int64{1, 2, 4, 4}, 8}, true, []int64{4, 4},
+	},
+}
+
+func TestGetPairs(t *testing.T) {
+	for _, structure := range tests {
+		success_value, slice_of_ints := GetPairsThatMatchesSum(structure.tests)
+		if success_value != structure.expected_value {
+			t.Error(
+				"Should have returned", structure.expected_value,
+				"instead of", success_value,
+			)
+		}
+		if slice_of_ints == nil && structure.expected_slice_of_ints == nil {
+			return
+		}
+		if slice_of_ints == nil || structure.expected_slice_of_ints == nil {
+			t.Fatal("For", structure.tests.values,
+				"was expected", structure.expected_slice_of_ints)
+		}
+		for key := range slice_of_ints {
+			if slice_of_ints[key] != structure.expected_slice_of_ints[key] {
+				t.Error(
+					"For", structure.tests.values,
+					"was expected", structure.expected_slice_of_ints,
+				)
+			}
+		}
+	}
+}
